ee7323174b571cedbb2205d4534bed3f81a46432
alejandrovarelasc <alejandro.varela@southerncode.us>
2024-08-15 17:11:09 -0500

    chore: Fix: Adding iac third approach

--- iac/main.tf
+++ iac/main.tf
@@ -7,6 +7,27 @@ module "networks" {
   private_subnet_cidrs = var.private_subnet_cidrs
 }
 
+################################
+#  ROLES FOR SERVICE ACCOUNTS  #
+################################
+
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name_prefix      = "VPC-CNI-IRSA"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -19,63 +40,47 @@ module "eks" {
   cluster_addons = {
     coredns                = {}
     eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
+    kube-proxy             = {
+      most_recent = true
+    }
+    vpc-cni                = {
+      most_recent              = true 
+      before_compute           = true
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
   }
 
-  vpc_id                   = "vpc-1234556abcdef"
-  subnet_ids               = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
-  control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+  vpc_id                   = module.networks.vpc_id
+  subnet_ids               = module.networks.private_nets
+  control_plane_subnet_ids = module.networks.private_nets
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t3.medium"]
+    iam_role_attach_cni_policy = true
   }
 
   eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
-
+    stw_node_wg = {
       min_size     = 2
-      max_size     = 10
+      max_size     = 6
       desired_size = 2
     }
-  }
-
-  # Cluster access entry
-  # To add the current caller identity as an administrator
-  enable_cluster_creator_admin_permissions = true
-
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::123456789012:role/something"
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            namespaces = ["default"]
-            type       = "namespace"
-          }
-        }
-      }
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
   }
 }
 
 module "security_group" {
   for_each = var.instances
   source               = "./modules/security/security-groups"
-  security_group_name  = "${each.value.instance_name}-sg"
+  security_group_name  = "${each.value.name}-sg"
   vpc_id               = module.networks.vpc_id
   allowed_ssh_cidrs    = var.allowed_ssh_cidrs
   allowed_http_cidrs   = var.allowed_http_cidrs
@@ -86,7 +91,7 @@ module "ec2_instance" {
   for_each = var.instances
   source            = "./modules/compute/ec2"
   instance_type     = each.value.instance_type
-  instance_name     = each.value.instance_name
+  instance_name     = each.value.name
   security_group_id = module.security_group[each.key].security_group_id
   ami_id            = var.ami_id
   key_name          = aws_key_pair.tf_key.key_name
